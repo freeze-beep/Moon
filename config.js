d3e5ab48477382aa1568d1f8e1240faff23f6eb2
freeze-beep <freezec852@gmail.com>
2026-02-21 17:20:45 +0200

    Create config.js

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    SESSION_ID: process.env.SESSION_ID || "", 
+    OWNER_NUMBER: "243976908862", // Remplace par ton numéro avec l'indicatif (ex: 243...)
+    PREFIX: ".",
+    BOT_NAME: "AYANOKOJI-BOT"
+};
